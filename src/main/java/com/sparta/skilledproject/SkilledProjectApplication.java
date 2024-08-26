800d5b72fe952c900b9e6769d21eeb8bbc53c930
KTJ-1650 <xowns1650@gmail.com>
2024-08-26 17:23:08 +0900

    Init

---
+++ src/main/java/com/sparta/skilledproject/SkilledProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.skilledproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkilledProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkilledProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/skilledproject/SkilledProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.skilledproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkilledProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
